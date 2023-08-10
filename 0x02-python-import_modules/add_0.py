66d9041567f065c27e1355234367595ba44f7d33
Sapphyruss <lugi.franklin@outlook.fr>
2023-08-10 05:30:16 -0700

    adding module

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        a + b
+    """
+    return (a + b)
