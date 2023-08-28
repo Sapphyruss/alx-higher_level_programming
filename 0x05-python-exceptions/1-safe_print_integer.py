898608f08294fdac6af88b93d6517ab30acd04b7
Sapphyruss <lugi.franklin@outlook.fr>
2023-08-28 13:09:37 -0700

    printint

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+# 1-safe_print_integer.py
+
+def safe_print_integer(value):
+    """Print an integer with "{:d}".format()."""
+
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
