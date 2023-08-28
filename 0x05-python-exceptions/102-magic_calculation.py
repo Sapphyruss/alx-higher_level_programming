6205e6767ba48792a3ec09b0ea865ad63f3d8705
Sapphyruss <lugi.franklin@outlook.fr>
2023-08-28 13:41:24 -0700

    do not use bare except

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,13 +2,13 @@
 
 def magic_calculation(a, b):
     result = 0
+
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception('Too far')
-            else:
-                result += a ** b / i
-        except:
-            result = b + a
+                raise Exception("Too far")
+            result += (a ** b) / i
+        except Exception:
+            result = a + b
             break
-    return (result)
+    return result
