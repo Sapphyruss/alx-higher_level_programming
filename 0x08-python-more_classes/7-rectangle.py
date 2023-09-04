6645b4d7563322b135da1f420ee67078a877bfd4
Sapphyruss <lugi.franklin@outlook.fr>
2023-09-04 11:36:26 -0700

    .

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -84,4 +84,4 @@ class Rectangle:
     def __del__(self):
         """Print a message for every deletion of a Rectangle."""
         type(self).number_of_instances -= 1
-        print("Byye rectangle...")
+        print("Bye rectangle...")
