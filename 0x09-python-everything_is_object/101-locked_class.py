96a93edd20a80d1c45e674bcc35098a77e9caef1
Sapphyruss <lugi.franklin@outlook.fr>
2023-09-05 09:24:37 -0700

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+"""Module prevents the user from creating new instances attributes"""
+
+
 class LockedClass:
-    __slots__ = ['first_name']
+    """Define a Locked class
+    """
+    __slots__ = ('first_name', )
+
+    def setattr(self, name, value):
+        """Defines the attribute
+
+        Args:
+            name : first_name
+            value : first_name
+        """
+        if name == 'first_name':
+            self.__dict__[name] = value
