687cd082cae7213e0bd3b8a05bf31e39d22ee134
Sapphyruss <lugi.franklin@outlook.fr>
2023-09-12 12:05:59 -0700

    .

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Define a text file line-counting function."""
+"""This module write to a txt file"""
 
 
-def number_of_lines(filename=""):
-    """number of lines from file
-    args:
-        filename: file to read
-    return:
-        number of lines
+def write_file(filename="", text=""):
+    """This function write to a txt file
+
+    Args:
+        filename : File to be written into to "".
+        text : Text to be written to "".
+
+    Returns:
+        int: number of characters written
     """
-    line_num = 0
-    with open(filename, encoding="utf-8") as f:
-        for line in f:
-            line_num += 1
-    return line_num
+    with open(filename, 'w', encoding='utf-8') as file:
+        return (file.write(text))
