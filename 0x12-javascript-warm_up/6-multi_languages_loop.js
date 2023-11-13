58bdaaf9150d987b11cb5126ec9b628150399864
Sapphyruss <lugi.franklin@outlook.fr>
2023-11-13 12:57:21 -0800

    .

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+// Prints 3 lines
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+while (i < 3) {
+  console.log(array[i]);
+  i++;
 }
