04dfd128decd45a64f2ce1690cc011f9cc175d93
Sapphyruss <lugi.franklin@outlook.fr>
2023-11-20 04:09:10 -0800

    .

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import MySQLdb
 
 def main():
     # make a connection to db
-    db = MySQLdb.connect(host='127.0.0.1',
+    db = MySQLdb.connect(host='localhost',
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
