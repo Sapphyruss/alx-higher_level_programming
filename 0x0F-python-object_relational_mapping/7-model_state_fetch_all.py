cbaab8025b345f8cee53b69cb85e628c2e7eec9f
Sapphyruss <lugi.franklin@outlook.fr>
2023-11-21 05:06:28 -0800

    .

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-# list all state objects from the db
+"""lists all State objects from database """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy.engine.url import URL
 
-    db = {'drivername': 'mysql+mysqldb',
-          'host': 'localhost',
-          'port': '3306',
-          'username': argv[1],
-          'password': argv[2],
-          'database': argv[3]}
-
-    url = URL(**db)
-    engine = create_engine(url, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-
+if __name__ == '__main__':
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]} \
+        @localhost/{argv[3]}', pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    rows = session.query(State).all()
+    for row in rows:
+        print(f'{row.id}: {row.name}')
     session.close()
