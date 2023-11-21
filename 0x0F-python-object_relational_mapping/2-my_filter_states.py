f11c274a9d4dfc890a265f175861fa0b1e3c1b1c
Sapphyruss <lugi.franklin@outlook.fr>
2023-11-21 05:36:11 -0800

    .

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,34 +1,15 @@
 #!/usr/bin/python3
-"""filter states by user input"""
+"""Write a script that takes in an argument"""
+from sys import argv
 import MySQLdb
 
 
-def main():
-
-    # connect
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    # cursor
-    c = db.cursor()
-
-    # execute query
-    c.execute("SELECT * FROM states WHERE BINARY name = '{:s}'\
-    ORDER BY id ASC".format(argv[4]))
-
-    # fetch
-    rows = c.fetchall()
-
-    # print
-    for row in rows:
-        print(row)
-
-    # close
-    c.close()
-    db.close()
-
 if __name__ == "__main__":
-    from sys import argv
-    main()
+    name = argv[1]
+    data = argv[3]
+    search = argv[4]
+    db = MySQLdb.connect(user=name, passwd=argv[2], db=data)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+        WHERE name LIKE BINARY '{}'".format(search))
+    [print(row) for row in cur.fetchall()]
