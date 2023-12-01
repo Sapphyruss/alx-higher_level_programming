2ed32b3969d7536162fe47ce84cc82de7641f10a
Sapphyruss <lugi.franklin@outlook.fr>
2023-12-01 05:18:47 -0800

    .

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Takes an URL, sends a GET request to URL and displays the body of response
+curl -sL "$1"
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Send DELETE request to URL and display body of response
+curl -s "$1" -X DELETE
