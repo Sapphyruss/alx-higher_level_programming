7f797752920a670d8ccb0a1665aec45bc29d0f23
Sapphyruss <lugi.franklin@outlook.fr>
2024-01-03 12:37:12 -0800

    .

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
   if (err) {
     console.log(err);
-    return;
   }
-  console.log(data);
 });
